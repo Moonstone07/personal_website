37103502d5107226d6519434769c45ebde71ee5e
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-02-06 18:03:37 -0500

    add touch events for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,52 @@ document.querySelectorAll(".accordion_info").forEach((item) => {
   item.addEventListener("click", () => {
     const currentAccordionItem = item.parentElement;
     const accordionContent =
-        currentAccordionItem.querySelector(".accordion_content");
-      
-      
-      //TOGGLE
-      currentAccordionItem.classList.toggle("show");
-      
-      
-      // Display or hide the accordion content when the other accordion item is clicked
-      
-      if (currentAccordionItem.classList.contains("show")) {
-          accordionContent.style.display = "block"
-      } else {
-          accordionContent.style.display = "none"
-      }
+      currentAccordionItem.querySelector(".accordion_content");
 
-      // keep the first accordion item open by default
-      document.querySelectorAll(".accordion_item").forEach((item) => {
-          if (item !== currentAccordionItem && item.classList.contains("show")) {
-              item.classList.remove("show");
-              item.querySelector(".accordion_content").style.display = "none";
-          }
-      });
-      
+    //TOGGLE
+    currentAccordionItem.classList.toggle("show");
+
+    // Display or hide the accordion content when the other accordion item is clicked
+
+    if (currentAccordionItem.classList.contains("show")) {
+      accordionContent.style.display = "block";
+    } else {
+      accordionContent.style.display = "none";
+    }
+
+    // keep the first accordion item open by default
+    document.querySelectorAll(".accordion_item").forEach((item) => {
+      if (item !== currentAccordionItem && item.classList.contains("show")) {
+        //   remove it and stop displaying the accordion content
+        item.classList.remove("show");
+        item.querySelector(".accordion_content").style.display = "none";
+      }
+    });
   });
 });
+
+// RESUME SLIDER
+
+// touch events for mobile devices
+
+slides.addEventListener("touchstart", (e) => {
+  let touchStartX = e.changedTouches[0].screenX;
+});
+
+slides.addEventListener("touchmove", (e) => {
+  let touchEndX = e.changedTouches[0].screenX;
+});
+
+slides.addEventListener("touchend", (e) => {
+  handleSwipe();
+});
+
+function handleSwipe() {
+  if (touchEndX < touchStartX) {
+    slides.scrollBy({ left: slides.clientWidth, behavior: "smooth" });
+  }
+  if (touchEndX > touchStartX) {
+    slides.scrollBy({ left: -slides.clientWidth, behavior: "smooth" });
+  }
+}
+// CIRCLES
