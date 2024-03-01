a198b76d8612a3a85840344b31f8575b9ba19dc9
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-03-01 18:21:57 -0500

    Update styles.css and script.js, add Locomotive Scroll library

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,9 +34,37 @@ document.querySelectorAll(".accordion_info").forEach((item) => {
 let slides, circles;
 
 document.addEventListener("DOMContentLoaded", function () {
+// Locomotive Scroll
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smoothMobile: true,
+  inertia: 0.3,
+});
+
+// Function to check if elements are in view and trigger animation
+function animateElements() {
+  const scrollY = scroll.scroll.instance.scroll.y;
+  const viewportHeight = window.innerHeight;
+  document.querySelectorAll(".fade_in_right, .fade_in_left, .fade_in, .slide_up, .fade_up").forEach((element) => {
+    const elementTop = element.offsetTop;
+    if (scrollY + viewportHeight >= elementTop) {
+      element.classList.add("show");
+    }
+  });
+}
+
+// Call animateElements function when page loads
+animateElements();
+
+// Call animateElements function when scroll event occurs
+scroll.on("scroll", animateElements);
+
+
   slides = document.querySelector(".slides");
   circles = document.querySelectorAll(".circle");
 
+  // update the circles when the slide changes
   function updateCircles(activeIndex) {
     circles.forEach((circle, index) => {
       if (index === activeIndex) {
@@ -120,3 +148,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 });
 
+
+// Locomotive Scroll
+
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("[data-scroll-container]"),
+//   smooth: true,
+//   smoothMobile: true,
+//   inertia: 0.3,
+// })
