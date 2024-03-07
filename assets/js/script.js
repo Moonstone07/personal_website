596b38bfb365b56612ff7b69e5dc098760a5d93d
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-03-07 13:05:04 -0500

    Refactor scroll functionality and add animation triggers

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,30 +38,30 @@ let slides, circles;
 
 document.addEventListener("DOMContentLoaded", function () {
 // Locomotive Scroll
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector("[data-scroll-container]"),
-//   smooth: true,
-//   smoothMobile: true,
-//   inertia: 0.3,
-// });
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smoothMobile: true,
+  inertia: 0.3,
+});
 
-// // Function to check if elements are in view and trigger animation
-// function animateElements() {
-//   const scrollY = scroll.scroll.instance.scroll.y;
-//   const viewportHeight = window.innerHeight;
-//   document.querySelectorAll(".fade_in_right, .fade_in_left, .fade_in, .slide_up, .fade_up").forEach((element) => {
-//     const elementTop = element.offsetTop;
-//     if (scrollY + viewportHeight >= elementTop) {
-//       element.classList.add("show");
-//     }
-//   });
-// }
+// Function to check if elements are in view and trigger animation
+function animateElements() {
+  const scrollY = scroll.scroll.instance.scroll.y;
+  const viewportHeight = window.innerHeight;
+  document.querySelectorAll(".fade_in_right, .fade_in_left, .fade_in, .slide_up, .fade_up").forEach((element) => {
+    const elementTop = element.offsetTop;
+    if (scrollY + viewportHeight >= elementTop) {
+      element.classList.add("show");
+    }
+  });
+}
 
-// // Call animateElements function when page loads
-// animateElements();
+// Call animateElements function when page loads
+animateElements();
 
-// // Call animateElements function when scroll event occurs
-// scroll.on("scroll", animateElements);
+// Call animateElements function when scroll event occurs
+scroll.on("scroll", animateElements);
 
 
   slides = document.querySelector(".slides");
