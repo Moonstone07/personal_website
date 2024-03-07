7d17e6e1ec2bb0e051f813809484e90df448d398
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-03-07 12:29:46 -0500

    Update transitions and margins in CSS, add offset top function in scroll.js, and fix language in index.html

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -6,21 +6,41 @@ const scroll = new LocomotiveScroll({
   smoothMobile: true,
   inertia: 0.3,
 });
+  
+  // Get the offset top of an element
+    function getOffsetTop(element) {
+      let offsetTop = 0;
+      while (element) {
+        offsetTop += element.offsetTop;
+        element = element.offsetParent;
+      }
+      return offsetTop;
+    }
 
 // Function to check if elements are in view and trigger animation
 function animateElements() {
   const scrollY = scroll.scroll.instance.scroll.y;
   const viewportHeight = window.innerHeight;
   document.querySelectorAll(".fade_in_right, .fade_in_left, .fade_in, .slide_up, .fade_up").forEach((element) => {
-    const elementTop = element.offsetTop;
-    if (scrollY + viewportHeight >= elementTop) {
+    let trigger = element;
+    if (element.querySelector('.trigger')) {
+      trigger = element.querySelector('.trigger');
+    }
+    const triggerTop = getOffsetTop(trigger);
+    const triggerBottom = triggerTop + trigger.offsetHeight;
+    if (scrollY + viewportHeight >= triggerTop && scrollY <= triggerBottom) {
       element.classList.add("show");
+    } else {
+      element.classList.remove("show");
     }
   });
 }
 
 // Call animateElements function when page loads
-animateElements();
+  animateElements();
+  
+// update Locomotive Scroll
+scroll.update();
 
 // Call animateElements function when scroll event occurs
 scroll.on("scroll", animateElements);
