8b2f92de820042869cbd3866ab302a0bed3d53e7
Tischa <84481096+Moonstone07@users.noreply.github.com>
2024-06-04 13:57:55 -0400

    Update font-size and remove unused code add menu js add default image

--- assets/js/photography_sticky.js
+++ assets/js/photography_sticky.js
@@ -73,19 +73,6 @@ const addEventListeners = () => {
 
 animate();
 
-// Get the menu and buttons elements
-let menu = document.getElementById('mobileMenu');
-let openButton = document.getElementById('openMenu');
-let closeButton = document.getElementById('closeMenu');
 
-// Add event listener for clicks
-document.addEventListener('click', function(event) {
-    if (event.target === openButton) {
-        menu.classList.add('open');
-        menu.style.display = 'block';
-    } else if (event.target === closeButton) {
-        menu.classList.remove('open');
-    }
-});
 
 
